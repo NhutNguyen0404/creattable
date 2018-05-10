6d038266570cf99299f1d12f14b55552e4e261c5
= <=>
2018-05-10 11:07:33 +0700

    Change proj. 10/05/18

--- resources/views/install/head.blade.php
+++ resources/views/install/head.blade.php
@@ -4,6 +4,7 @@
 <link rel="stylesheet" href=".\public\css\bootstrap.min.css" />
 <link rel="stylesheet" href=".\public\css\reset.css" />
 <link rel="stylesheet" href=".\public\css\install_style.css" />
+<script type="text/javascript" src=".\public\js\jquery-3.3.1.min.js"></script>
 <script type="text/javascript" src=".\public\js\bootstrap.min.js"></script>
 <script type="text/javascript" src=".\public\js\lib_custom.min.js"></script>
 </head>
